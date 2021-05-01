bad1389413c6c254e50ade51760b68efee244ced
gerlinp <gerlin.pierrelouis@gmail.com>
2021-04-30 23:11:17 -0400

    reusabale fetch function

--- fetch_api/2- Write a Basic Fetch Request/start/js/app.js
+++ fetch_api/2- Write a Basic Fetch Request/start/js/app.js
@@ -5,13 +5,40 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+function fetchData(url) {
+    return fetch(url)
+        .then(res => res.json())
+}
+
+fetchData('https://dog.ceo/api/breeds/list')
+
+    .then(data => generateOptions(data.message))
+
+
+fetchData('https://dog.ceo/api/breeds/image/random')
+
+    .then(data => generateImage(data.message))
 
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function generateOptions(data) {
+    const options = data.map(item => `
+        <option vlaue='${item}'>${item}</option>
+    `).join('');
+    select.innerHTML = options;
+}
 
+
+function generateImage(data) {
+    const html = `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+    `;
+    card.innerHTML = html;
+}
 
 
 // ------------------------------------------
